7c6c89085a68297d6ad435143c724339c38ee303
deejnv007 <84219579+deejnv007@users.noreply.github.com>
2022-04-13 00:19:08 +0530

    Add files via upload

---
+++ SingleDArray.java
@@ -0,0 +1,21 @@
+package array;
+
+public class SingleDArray {
+
+	public static void main(String[] args) {
+//		int [] marks = new int [13]; 
+//		System.out.println(marks[12]);
+		int rollnos[],classes[];
+		int [] age = {12,5,21,9};
+		double marks[] = {2.0,34.5,18.9};
+		marks[1] = 49.0;
+		System.out.println(marks[1]);
+		System.out.println(age[3]);
+		for(int i=0;i<age.length;i++) {
+			System.out.println(age[i]);
+		}
+	  }
+
+	}
+
+
