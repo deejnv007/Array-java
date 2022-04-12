0a9d5376f86d1f0adbf88c954c268da36e8fa82b
deejnv007 <84219579+deejnv007@users.noreply.github.com>
2022-04-13 00:19:26 +0530

    Add files via upload

---
+++ MultiDArray.java
@@ -0,0 +1,17 @@
+package array;
+
+public class MultiDArray {
+
+	public static void main(String[] args) {
+//		int a[][] = new int[2][3];
+//		System.out.println(a[1][2]);
+		int a[][] = {
+				{1, 2, 3,},
+				{4, 5, 6,} 
+		};
+		System.out.println(a[1][2]);
+		
+
+	}
+
+}
